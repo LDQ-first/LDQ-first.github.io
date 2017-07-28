cc133719200c0ccc3a1ffa857c3998834fbd63ac
LDQ-first <178848890@qq.com>
2017-07-28 23:33:17 +0800

    modify theme

--- scripts/image-stream.js
+++ scripts/image-stream.js
@@ -48,47 +48,5 @@ hexo.extend.tag.register('figure', function(args){
   return result;
 });
 
-
-hexo.extend.tag.register('timemachine', function(args, content){
-  var result = '';
-  if (config['jquery']) {
-    result += '<script src="' + config['jquery'] + '"></script>';
-  }
-  if (config['jquery_lazyload']) {
-    result += '<script src="' + config['jquery_lazyload'] + '"></script>';
-  }
-  result += '<div class="hexo-img-timemachine">';
-  result += '<style type="text/css">';
-  result += '.hexo-image-steam-lazy {display:block;}.hexo-img-timemachine{width:100%;max-width:1100px;margin:3% auto}div.hexo-img-timemachine part{background:#fefefe;box-shadow:0 1px 2px rgba(34, 25, 25, 0.2);margin:0 1% 3%;padding:2.5%;padding-bottom:10px;display:inline-block;max-width:42%}div.hexo-img-timemachine part img{padding-bottom:10px;margin-bottom:5px}div.hexo-img-timemachine part figcaption{font-size:.7rem;color:#999;line-height:1.5;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;}figcaption-t{font-size:.7rem;color:#c7c7c7;line-height:1.5;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;}div.hexo-img-timemachine small{font-size:1rem;float:right;text-transform:uppercase;color:#aaa}div.hexo-img-timemachine small a{color:#666;text-decoration:none;transition:.4s color}@media screen and (max-width:750px){.hexo-img-timemachine{column-gap:0}}';
-  result += '</style>';
-  result += content;
-  result += '</div>';
-  result += '<script type="text/javascript">$(\'img.hexo-image-steam-lazy\').lazyload({ effect:\'fadeIn\' });</script>';
-  return result;
-}, {ends: true});
-
-hexo.extend.tag.register('part', function(args){
-  var imgUrl = args.shift();
-  //var title = args.join(' ');
-  var placeholder = config['img_placeholder'];
-  var wordUrl = args.shift();
-  var title = args.shift();
-  var time = args.shift();
-  
-  var result = '<part>';
-  result += '<a href="' + wordUrl + '" target="_blank" rel="external">'
-  result += '<img class="hexo-image-steam-lazy nofancy" src="' + placeholder + '" data-original="' + imgUrl + '"/>';
-  result += '<noscript><img src="' + imgUrl + '"/></noscript>' + '</a>';
-  result += '<figcaption>'+title+'</figcaption>' ;
-  result += '<figcaption-t>'+time+'</figcaption-t>' + '</a>';
-  
-  
-  
-  
-//   result += '<img class="hexo-image-steam-lazy nofancy" src="' + placeholder + '" data-original="' + imgUrl + '"/>';
-//   result += '<noscript><img src="' + imgUrl + '"/></noscript>';
-//   result += '<figcaption>' + hexo.render.renderSync({text: title, engine: 'markdown'}).replace(/<p>/, '').replace(/<.p>/, '') + '</figcaption>';
-  result += '</part>';
-  return result;
-});
+);
 
