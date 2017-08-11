ae280a929a56163796ab52bdd1d2f7e3b143a3d1
LDQ-first <178848890@qq.com>
2017-08-11 09:28:53 +0800

    'modify'

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,13 @@ const minifycss = require('gulp-minify-css');
 const uglify = require('gulp-uglify');
 const htmlmin = require('gulp-htmlmin');
 const htmlclean = require('gulp-htmlclean');
+const amdOptimize = require("amd-optimize"); 
 
 // 压缩 public 目录 css文件
 gulp.task('minify-css', function() {
     return gulp.src('./public/**/*.css')
         .pipe(minifycss())
-        .pipe(gulp.dest('./public'));
+        .pipe(gulp.dest('./public/css'));
 });
 
 // 压缩 public 目录 html文件
@@ -26,16 +27,41 @@ gulp.task('minify-html', function() {
     .pipe(gulp.dest('./public'))
 });
 
+
+gulp.task("main", function () {  
+  return gulp.src("./public/js/*.js")   
+    // Traces all modules and outputs them in the correct order.   
+    .pipe(amdOptimize("main"))   //主入口文件    
+    .pipe(uglify()) 
+    .pipe(gulp.dest("./public/js"))  //输出目录  
+});  
+
+gulp.task("pc", function () {  
+  return gulp.src("./public/js/*.js")   
+    // Traces all modules and outputs them in the correct order.   
+    .pipe(amdOptimize("pc"))   //主入口文件    
+    .pipe(uglify()) 
+    .pipe(gulp.dest("./public/js"))  //输出目录  
+}); 
+
+gulp.task("mobile", function () {  
+  return gulp.src("./public/js/*.js")   
+    // Traces all modules and outputs them in the correct order.   
+    .pipe(amdOptimize("mobile"))   //主入口文件    
+    .pipe(uglify()) 
+    .pipe(gulp.dest("./public/js"))  //输出目录  
+}); 
+
 // 压缩 public/js 目录 js文件
 // es6语法导致出错？
 gulp.task('minify-js', function() {
     return gulp.src('./public/js/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest('./public'));
+        .pipe(gulp.dest('./public/js'));
 });
 
 
 // 执行 gulp 命令时执行的任务
 gulp.task('default', [
-    'minify-html','minify-css'
+    'minify-html','minify-css', 'main', 'pc', 'mobile','minify-js'
 ]);
