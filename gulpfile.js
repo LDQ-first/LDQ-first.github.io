b20fa3c04dc699b8e2e13366367eeb3f44f07bcc
LDQ-first <178848890@qq.com>
2017-08-11 11:14:32 +0800

    'modify'

--- gulpfile.js
+++ gulpfile.js
@@ -40,14 +40,14 @@ gulp.task('minify-html', function() {
 
 
 gulp.task('img', function(){
-    return gulp.src('./public/**.*')
+    return gulp.src('./public/**/**.*')
                .pipe(imagemin ({
                     optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
                     progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
                     interlaced: true, //类型：Boolean 默认：false 隔行扫描gif进行渲染
                     multipass: true //类型：Boolean 默认：false 多次优化svg直到完全优化
                }))
-            .pipe(gulp.dest('dist'));
+            .pipe(gulp.dest('./public'));
 })
 
 
